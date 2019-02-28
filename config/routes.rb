e78612f01f5b36dc0fb5e2203a742d798fb2d97e
gontabi <kanehisamasaharu@kanehisamasaharu-no-MacBook-Air.local>
2019-02-28 15:42:55 +0900

    Correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 <<<<<<< HEAD
   devise_for :users
   root 'groups#index'
-  # root 'messages#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
