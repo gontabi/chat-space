41b97dd37a46b645c913b0f630d75ba16ad11d27
gontabi <kanehisamasaharu@kanehisamasaharu-no-MacBook-Air.local>
2019-03-05 15:07:52 +0900

    asynchronous1

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function(){
+  function buildHTML(message){
+    var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+    var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                      ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                      ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                      ${message.content}
+                      </p>
+                      ${imagehtml}
+                    </div>
+                  </div> `
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+     $( ".form__submit").prop( "disabled", false );
+
+     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+     $('.form__message').val('');
+     $('.hidden').val('');
+    })
+   .fail(function(){
+      alert('error');
+    })
+  })
+});
